d13a578fb7453e4ff251422cd6d4cb852ea39ed1
Al Amin Khan <alaminkhan2010@gmail.com>
2025-11-10 13:35:13 +0600

    build: set up project with npm init with: - add express, mongodb, nodemon, cors, dotenv - implement mongodb connection with ping - add .env and index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const username = process.env.mongodb_user;
+const password = process.env.mongodb_password;
+const appName = process.env.appName;
+
+
+const uri = `mongodb+srv://${username}:${password}@fastpay-aws-mumbai-clus.ktdeaau.mongodb.net/?appName=${appName}`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+        const db = client.db("fastPayDB");
+        const billsCollection = db.collection("bills");
+        const myBillsCollection = db.collection("myBills");
+
+
+        console.log("Connected to MongoDB");
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+
+        
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        process.exit(1);
+    }
+};
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Smart Deal server is running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
